4201ab755df6f1a6da5c76c3c1ef6f3c77b126af
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-12-17 01:06:33 +0200

    add cases with async await

--- tasks/task2/task2.js
+++ tasks/task2/task2.js
@@ -49,3 +49,40 @@ async function demoFunc() {
             console.error('Error in case 2:', error);
         });
 }
+demoFunc();
+
+// Use cases with async-await
+function promiseMap(array, callback) {
+    const promises = array.map(item => callback(item));
+    return Promise.all(promises);
+}
+
+async function demoFunc() {
+
+    // case 1
+    const numbers = [3, 6, 9, 12];
+
+    const promiseTriple = (num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(num * 2), Math.random() * 1000);
+        });
+    };
+
+    const results1 = await promiseMap(numbers, promiseTriple);
+    console.log('Async-await Case 1 Results:', results1);
+
+
+    // case 2
+    const numbers2 = [12, 24, 36];
+
+    const promiseSquare = (num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(num ** 2), Math.random() * 1000);
+        });
+    };
+
+    const results2 = await promiseMap(numbers2, promiseSquare);
+    console.log('Async-await Case 2 Results:', results2);
+}
+
+demoFunc();
