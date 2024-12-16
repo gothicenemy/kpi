53b871259215673512e3268c65cdafc7bd0db1c7
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-12-17 01:02:10 +0200

    Add the first case with an array of numbers and the promiseTriple

--- tasks/task2/task2.js
+++ tasks/task2/task2.js
@@ -3,3 +3,27 @@ function promiseMap(array, callback) {
     return Promise.all(promises);
 }
 async function demoFunc() {
+
+    // case 1
+    const numbers = [2, 4, 6, 8, 10];
+
+    const promiseTriple = (num) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (num === 6) {
+                    reject(`Warning: Number ${num} cannot be processed`);
+                } else {
+                    resolve(num * 2);
+                }
+            }, Math.random() * 1000);
+        });
+    };
+
+    promiseMap(numbers, promiseTriple)
+        .then(results => {
+            console.log('Case 1 Results:', results);
+        })
+        .catch(error => {
+            console.error('Error in case 1:', error);
+        });
+}
