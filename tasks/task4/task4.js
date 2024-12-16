8a8a2ed499ab50165b6e71fe3396a3ca12041c61
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-12-17 01:32:12 +0200

    create demoFunc with abort signal handling

--- tasks/task4/task4.js
+++ tasks/task4/task4.js
@@ -19,3 +19,28 @@ async function asyncMap(asyncIterable, callback, signal) {
 
     return res;
 }
+async function demoFunc() {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    const numbers = [10, 20, 30, 40, 50];
+    const promiseTriple = (num) => Promise.resolve(num * 3);
+
+    try {
+        const iterable = asyncGenerator(numbers);
+
+        setTimeout(() => {
+            console.log("Aborting the process...");
+            controller.abort();
+        }, 1800);
+
+        const res1 = await asyncMap(iterable, promiseTriple, signal);
+        console.log("Final results:", res1);
+    } catch (err) {
+        if (err.name === "Abort_Error") {
+            console.error("Process was aborted successfully");
+        } else {
+            console.error("Unexpected Error:", err);
+        }
+    }
+}
