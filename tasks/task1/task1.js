1614d641c5152d1adbafbf5c10f6431f25231c49
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-12-17 00:44:49 +0200

    Added implementation of mapWithCallback function with error handling and final callback

--- tasks/task1/task1.js
+++ tasks/task1/task1.js
@@ -2,3 +2,23 @@ function mapWithCallback(array, callback, completionCallback) {
     const results = [];
     let processedItems = 0;
 
+    function mapWithCallback(array, callback, completionCallback) {
+        const results = [];
+        let processedItems = 0;
+
+        array.forEach((value, idx) => {
+            callback(value, (err, result) => {
+                if (err) {
+                    console.error(`Error occurred: ${err}`);
+                    results[idx] = null;
+                } else {
+                    results[idx] = result;
+                }
+
+                processedItems++;
+                if (processedItems === array.length) {
+                    completionCallback(results);
+                }
+            });
+        });
+    }
