0f9bd27a13529bb320504be442d500cde4cd267c
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-12-17 00:38:27 +0200

    mapwithcallback

---
+++ tasks/task1/task1.js
@@ -0,0 +1,20 @@
+function mapWithCallback(array, callback, completionCallback) {
+    const results = [];
+    let processedItems = 0;
+
+    array.forEach((value, idx) => {
+        callback(value, (err, result) => {
+            if (err) {
+                console.error(`Error occurred: ${err}`);
+                results[idx] = null;
+            } else {
+                results[idx] = result;
+            }
+
+            processedItems++;
+            if (processedItems === array.length) {
+                completionCallback(results);
+            }
+        });
+    });
+}
